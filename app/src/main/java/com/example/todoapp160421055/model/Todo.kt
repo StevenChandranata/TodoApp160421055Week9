d693fc75abe40af48deb4d6fb5a7e9b388dc9a46
StevenChandranata <stevenchandranata22@gmail.com>
2024-05-16 14:59:53 +0700

    week 9 ANMP

--- app/src/main/java/com/example/todoapp160421055/model/Todo.kt
+++ app/src/main/java/com/example/todoapp160421055/model/Todo.kt
@@ -11,8 +11,15 @@ data class Todo(
     @ColumnInfo(name="notes")
     var notes:String,
     @ColumnInfo(name="priority")
-    var priority:Int
+    var priority:Int,
+    @ColumnInfo(name="is_done")
+    var isDone: Int = 0 // artinya isDone == false
+    // karena Room (bagian dari android jetpack yang digunakan untuk menyederhanakan  penggunaan SQLite dalam aplikasi Android) tidak mendukung tipe BOOLEAN,sehingga nilai boolean dibuat dalam bentuk INTEGER yang mana nilai 0 sebagai false dan nilai 1 sebagai true didalam database.
 )  {
     @PrimaryKey(autoGenerate = true)
     var uuid:Int =0
+
+    fun markAsDone() {
+        isDone = 1 //artinya isDone == True
+    }
 }
